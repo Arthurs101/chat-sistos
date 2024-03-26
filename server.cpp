9eafd1df99c10dda3a185294c2e8452cf25e2b05
arthur <arg21527@uvg.edu.gt>
2024-03-25 20:04:12 -0600

    added rest of functions, can't compile cause of proto

--- server.cpp
+++ server.cpp
@@ -53,7 +53,7 @@ void *requestsHandler(void *params){
     struct Cli client;
     struct Cli *newClient = (struct Cli *)params; 
     int socket = newClient->socket; 
-    char buffer[8000];
+    char buffer[8500];
 
     // Server Structs
     string msgServer;
@@ -86,6 +86,143 @@ void *requestsHandler(void *params){
 				servingCLients[client.username] = &client;
 				break;
 			}
+            case 2:{ //ask for all users
+                if(request->users().user()!= "" || !request->users().has_user()){ //empty or it has no parameter
+                    chat::ConnectedUsersResponse *users = new chat::ConnectedUsersResponse();
+                    for(auto i:servingCLients){
+                        chat::UserInfo *user_in_pos = new chat::UserInfo();
+                        user_in_pos->set_username(i.second->username);
+                        user_in_pos->set_status(i.second->status);
+                        user_in_pos->set_ip(i.second->ip);
+                        users->add_connectedusers()->CopyFrom(*user_in_pos);
+                    }
+                    response->set_servermessage("SUCCESS: userinfo of all connected clients");
+                    response->set_allocated_connectedusers(users);
+                    response->set_option(2);
+                    response->set_code(200);
+                    response->SerializeToString(&msgServer);
+                    strcpy(buffer, msgServer.c_str());
+                    send(socket, buffer, msgServer.size() + 1, 0);
+                    std::cout<<"User:"<<client.username<<"requested all conected";
+                }
+                else{
+                    ErrorResponse(2,socket,"ERROR: can't specify a user when asking all");                
+                }
+				break;
+			}
+			case 3:{ //change status
+                try{
+                servingCLients[request->change().username()]->status = request->change().status();
+                std::cout<<"\n__USER CHANGE STATUS SOLICITUDE__\nUser: "<<client.username<<" requested to change status.\nSUCCESS:status has changed successfully\n";
+                chat::ChangeStatus *sStatus = new chat::ChangeStatus();
+                sStatus->set_username(request->change().username());
+                sStatus->set_status(request->change().status());
+                response->set_allocated_change(sStatus);
+                response->set_servermessage("\nSUCCESS:\nUser: "+request->change().username()+" has changed status successfully\n");
+                response->set_code(200);
+                response->set_option(3);
+                response->SerializeToString(&msgServer);
+                strcpy(buffer, msgServer.c_str());
+                send(socket, buffer, msgServer.size() + 1, 0);
+                }
+                catch(exception &e){
+                    ErrorResponse(3,socket,"Change status on: " + request->change().username() + "doesn't exist");
+                }
+				break;
+			}
+			case 4:{ //enviar mensaje
+                if(!request->messagecommunication().has_recipient()){ //a chat global
+                    std::cout<<"\n__SENDING GENERAL MESSAGE__\nUser: "<<request->messagecommunication().sender()<<" is trying to send a general message";
+                    for (auto i:servingCLients){
+                        if (i.first==request->messagecommunication().sender()){
+                            chat::MessageCommunication *response_message = new chat::MessageCommunication();
+                            response_message->set_message("");
+                            response->set_allocated_messagecommunication(response_message);
+                            response->set_servermessage("\nSUCCESS: general chat\n");
+                            response->set_code(200);
+                            response->set_option(4);
+                            response->SerializeToString(&msgServer);
+                            strcpy(buffer, msgServer.c_str());
+                            send(socket, buffer, msgServer.size() + 1, 0);
+                            std::cout<<"\nSUCCESS:User: "+request->messagecommunication().sender()+" has sent the message successfully to the general chat\n";
+                        }
+                        else{ 
+                            chat::MessageCommunication *message = new chat::MessageCommunication();
+                            message->set_sender(client.username);
+                            message->set_message(request->messagecommunication().message());
+                            response->set_allocated_messagecommunication(message);
+                            response->set_servermessage("\nUser: "+request->messagecommunication().sender()+" sends you a message\n");
+                            response->set_code(200);
+                            response->set_option(4);
+                            response->SerializeToString(&msgServer);
+                            strcpy(buffer, msgServer.c_str());
+                            send(i.second->socket, buffer, msgServer.size() + 1, 0);
+                        }
+                    }
+                }
+                else{ //is a direct message
+                    std::cout<<"\n__SENDING PRIVATE MESSAGE__\nUser: "<<request->messagecommunication().sender()<<" is trying to send a private message to ->"<<request->messagecommunication().recipient();
+                    auto _ = servingCLients.find(request->messagecommunication().recipient()); 
+                    if (_ != servingCLients.end()){
+                        chat::MessageCommunication *message = new chat::MessageCommunication();
+                        message->set_sender(client.username);
+                        message->set_recipient(request->messagecommunication().recipient());
+                        message->set_message(request->messagecommunication().message());
+                        response->set_allocated_messagecommunication(message);
+                        response->set_servermessage("\nUser: "+request->messagecommunication().sender()+" sends you a message\n");
+                        response->set_code(200);
+                        response->set_option(4);
+                        response->SerializeToString(&msgServer);
+                        strcpy(buffer, msgServer.c_str());
+                        send(servingCLients[request->messagecommunication().recipient()]->socket, buffer, msgServer.size() + 1, 0);
+                        response->Clear();
+                        //inform the sender that it was a succes
+                        chat::MessageCommunication *response_message = new chat::MessageCommunication();
+                        response_message->set_message("");
+                        response->set_allocated_messagecommunication(response_message);
+                        response->set_servermessage("SUCCESS: sent message to"+request->messagecommunication().recipient()+" successfully\n");
+                        response->set_code(200);
+                        response->set_option(4);
+                        response->SerializeToString(&msgServer);
+                        strcpy(buffer, msgServer.c_str());
+                        send(socket, buffer, msgServer.size() + 1, 0);
+                        std::cout<<"\nSUCCESS:User: "+request->messagecommunication().sender()+" has sent the message successfully ->"+request->messagecommunication().recipient()+"\n";
+                    }
+                    else{ // no existe
+                        ErrorResponse(4,socket,"ERROR: recipient doesn't exist" );
+                        cout<<"E: " + request->messagecommunication().sender()+ "tried to send to unexisting user: "+request->messagecommunication().recipient()<<endl;
+                    }
+                }
+				break;
+			}
+			case 5:{ //obtener usuario en específico
+				chat::UserInfo *userI = new chat::UserInfo();
+                try{
+                    //intentar obtener dicho valor con la llave (username)
+                    userI->set_username(servingCLients[request->users().user()]->username);                    
+                    userI->set_ip(servingCLients[request->users().user()]->ip);
+                    userI->set_status(servingCLients[request->users().user()]->status);
+                    response->set_allocated_userinforesponse(userI);
+                    response->set_servermessage("SUCCESS: userinfo of "+request->users().user());
+                    response->set_code(200);
+                    response->set_option(2);
+                    response->SerializeToString(&msgServer);
+                    strcpy(buffer, msgServer.c_str());
+                    send(socket, buffer, msgServer.size() + 1, 0);
+                    std::cout<<"\n__USER INFO SOLICITUDE__\nUser: "<<client.username<<" requested info of ->"<<request->users().user()<<"\nSUCCESS: userinfo of "<<request->users().user()<<std::endl;
+                }
+                catch(const std::exception& e){ //no existe el usuario
+                    response->set_servermessage("ERROR: "+request->users().user() + "doesn't exists");
+                    response->set_code(400);
+                    response->SerializeToString(&msgServer);
+                    strcpy(buffer, msgServer.c_str());
+                    send(socket, buffer, msgServer.size() + 1, 0);
+                    std::cout<<"\n__USER INFO SOLICITUDE__\nUser: "<<client.username<<" requested info of ->"<<request->users().user()<<"\nERROR: userinfo of "<<request->users().user()<<std::endl;
+                }
+			}
+			default:{
+				break;
+			}
         }
     }
 }
@@ -94,7 +231,7 @@ int main(int argc, char const* argv[]){
     //verificar versiión de protobuf para evitar errores
     GOOGLE_PROTOBUF_VERIFY_VERSION;
     if (argc != 2){
-        fprintf(stderr, "NO PORT ENABLED: server <server port>\n");
+        fprintf(stderr, "NO PORT DECLARED: server <port>\n");
         return 1;
     }
     long port = strtol(argv[1], NULL, 10);
