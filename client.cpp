3b287481cbe213dbe1b6049251e9bd8dcd4629f5
arthur <arg21527@uvg.edu.gt>
2024-04-01 12:37:34 -0600

    Revert "multithreading y progra defensiva"
    This reverts commit 0335a4d0591b4d381004482cb32c4ffd297a155f.
    
    Rverting the commit since it broke the parsing

--- client.cpp
+++ client.cpp
@@ -17,22 +17,20 @@ string nombre_usuario;
 void recibirMensajes() {
     while (true) {
         char buffer[8192];
-        int bytes_recibidos = recv(sockfd, buffer, sizeof(buffer), 0);
+        int bytes_recibidos = recv(sockfd, buffer, 8192, 0);
         if (bytes_recibidos == -1) {
             perror("recv fallido");
-            close(sockfd);
-            exit(EXIT_FAILURE);
+            break;
         } else if (bytes_recibidos == 0) {
             cout << "El servidor se desconectó." << endl;
             close(sockfd);
-            exit(EXIT_SUCCESS);
+            exit(1);
         } else {
             // Parsear respuesta
             chat::ServerResponse respuesta;
-            if (!respuesta.ParseFromArray(buffer, bytes_recibidos)) {
+            if (!respuesta.ParseFromString(buffer)) {
                 cerr << "Fallo al parsear la respuesta.\n aborting" << endl;
-                close(sockfd);
-                exit(EXIT_FAILURE);
+                break;
             }
             // Mostrar mensaje recibido
             cout <<"-------------------INCOMMING TRANSMISSION-------------------" << endl;
@@ -104,7 +102,7 @@ void listarUsuarios() {
     }
 
     chat::ServerResponse response;
-    int bytes_recibidos = recv(sockfd, buffer, sizeof(buffer), 0);
+    int bytes_recibidos = recv(sockfd, buffer, 8192, 0);
     if (bytes_recibidos == -1) {
         perror("recv fallido");
         return;
@@ -145,7 +143,7 @@ void obtenerInfoUsuario(const string& nombre_usuario) {
     }
 
     chat::ServerResponse response;
-    int bytes_recibidos = recv(sockfd, buffer, sizeof(buffer), 0);
+    int bytes_recibidos = recv(sockfd, buffer, 8192, 0);
     if (bytes_recibidos == -1) {
         perror("recv fallido");
         return;
@@ -202,7 +200,7 @@ void manejarEntradaUsuario() {
         } else if (opcion == "5") {
             cout << "Saliendo del chat." << endl;
             close(sockfd);
-            exit(EXIT_SUCCESS);
+            exit(0);
         } else {
             cout << "Opción no válida." << endl;
         }
@@ -212,24 +210,17 @@ void manejarEntradaUsuario() {
 int main(int argc, char const *argv[]) {
     if (argc != 4) {
         cerr << "Uso: " << argv[0] << " <nombredeusuario> <IPdelservidor> <puertodelservidor>" << endl;
-        return EXIT_FAILURE;
+        return 1;
     }
 
     nombre_usuario = argv[1];
     string ip_servidor = argv[2];
     int puerto_servidor = stoi(argv[3]);
 
-    // Validación de la dirección IP del servidor
-    struct sockaddr_in sa;
-    if (inet_pton(AF_INET, ip_servidor.c_str(), &(sa.sin_addr)) == 0) {
-        cerr << "Dirección IP del servidor inválida." << endl;
-        return EXIT_FAILURE;
-    }
-
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd == -1) {
         perror("socket fallido");
-        return EXIT_FAILURE;
+        return 1;
     }
 
     sockaddr_in server_addr;
@@ -239,12 +230,12 @@ int main(int argc, char const *argv[]) {
 
     if (connect(sockfd, (sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
         perror("connect fallido");
-        return EXIT_FAILURE;
+        return 1;
     }
 
     // Registrar al usuario con el servidor
     chat::UserRegistration new_user;
-    new_user.set_ip("10.0.2.15"); // OJO: Aquí podría obtenerse la IP real del cliente
+    new_user.set_ip("10.0.2.15");
     new_user.set_username(nombre_usuario);
 
     chat::ClientPetition request;
@@ -255,7 +246,7 @@ int main(int argc, char const *argv[]) {
     if (!request.SerializeToString(&mensaje_serializado)) {
         cerr << "Fallo al serializar el registro del usuario." << endl;
         close(sockfd);
-        return EXIT_FAILURE;
+        return 1;
     }
 
     char buffer[8192];
@@ -269,5 +260,5 @@ int main(int argc, char const *argv[]) {
     // Manejar la entrada del usuario
     manejarEntradaUsuario();
 
-    return EXIT_SUCCESS;
+    return 0;
 }
